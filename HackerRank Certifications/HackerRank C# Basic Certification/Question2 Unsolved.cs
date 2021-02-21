af2d305e4d106f5f68ac870fad86fc5d6dea96b5
ARNAB DAS <raj713335@gmail.com>
2021-02-21 16:58:29 +0530

    Add files via upload

---
+++ HackerRank Certifications/HackerRank C# Basic Certification/Question2 Unsolved.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Solution
+{
+
+    public class NotesStore
+    {
+        public NotesStore() { }
+        public void AddNote(String state, String name)
+        {
+            if (name == "" || name ==null)
+            {
+                throw new ArgumentException("Name Cannot be empty");
+            }
+            if (state != "active" || state != "completed" || state != "others")
+            {
+                throw new ArgumentException("Invalid state {0}", state);
+            }
+
+        }
+        public List<String> GetNotes(String state) { }
+    }
+
+    public class Solution
+    {
+        public static void Main()
+        {
+            var notesStoreObj = new NotesStore();
+            var n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i++)
+            {
+                var operationInfo = Console.ReadLine().Split(' ');
+                try
+                {
+                    if (operationInfo[0] == "AddNote")
+                        notesStoreObj.AddNote(operationInfo[1], operationInfo.Length == 2 ? "" : operationInfo[2]);
+                    else if (operationInfo[0] == "GetNotes")
+                    {
+                        var result = notesStoreObj.GetNotes(operationInfo[1]);
+                        if (result.Count == 0)
+                            Console.WriteLine("No Notes");
+                        else
+                            Console.WriteLine(string.Join(",", result));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Parameter");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+    }
+}
